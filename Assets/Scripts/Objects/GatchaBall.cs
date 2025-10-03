5c83cfde5a988b27b9457e00fa98c05b7b8b4189
Charles Mossop <charliemossop2235@gmail.com>
2025-10-03 12:45:52 +1000

    Hat board and confetti

--- Assets/Scripts/Objects/GatchaBall.cs
+++ Assets/Scripts/Objects/GatchaBall.cs
@@ -1,15 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
 public class GatchaBall : InteractableRigidbody
 {
+    [SerializeField] GameObject particleObject;
     public override void ActivateObject(PhysicsGrabber grabber)
     {
         base.ActivateObject(grabber);
         //spawn hat
 
+
         GameObject instance = Instantiate(HatBoard.instance.RetrieveHat());
         instance.transform.position = transform.position;
 
+        GameObject particle = Instantiate(particleObject);
+        particle.transform.position = transform.position + transform.up * 0.2f;
+
         Destroy(gameObject);
     }
+
 }
